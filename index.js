3141af9222ea834ad732b416faf18973e240de54
Abdulsemiu Atanda <semiu@live.com>
2019-03-08 17:22:25 +0100

    add Home component

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Header from './src/components/Header.react'
+import Home from './src/components/Home.react'
 import StyleGuide from './src/components/styleguide/StyleGuide.react'
 
 import './index.scss'
@@ -12,7 +13,7 @@ const Root = () => (
     <div>
       <Header />
       <Switch>
-        <Route exact path='/' component={() => (<h1>Home</h1>)} />
+        <Route exact path='/' component={Home} />
         <Route path='/style-guide' component={StyleGuide} />
       </Switch>
     </div>
