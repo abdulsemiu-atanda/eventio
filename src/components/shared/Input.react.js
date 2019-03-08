4d63a550ea22ced0433f6bfbaca837bc7b1467f3
Abdulsemiu Atanda <semiu@live.com>
2019-03-08 23:50:31 +0100

    fix bug where clicking label does not focus input

--- src/components/shared/Input.react.js
+++ src/components/shared/Input.react.js
@@ -4,22 +4,36 @@ import PropTypes from 'prop-types'
 
 import './input.scss'
 
-export const Input = ({className, ...otherProps}) => (
+export const Input = React.forwardRef(({className, ...otherProps}, ref) => (
   <input
+    ref={ref}
     className={classnames('base', className)}
     type='text'
     {...otherProps}
   />
-)
+))
 
 Input.propTypes = {className: PropTypes.string}
 
-export const FloatingLabelInput = ({className, label, ...otherProps}) => (
-  <div className='floating-label'>
-    <Input className={classnames('input-control', className)} {...otherProps} required />
-    {label && <label className='placeholder'>{label}</label>}
-  </div>
-)
+export class FloatingLabelInput extends React.Component {
+  constructor() {
+    super()
+
+    this.onClickLabel = this.onClickLabel.bind(this)
+  }
+
+  onClickLabel() { this.refs.input.focus() }
+
+  render() {
+    const {className, label, ...otherProps} = this.props
+    return (
+      <div className='floating-label'>
+        <Input ref='input' className={classnames('input-control', className)} {...otherProps} required />
+        {label && <label className='placeholder' onClick={this.onClickLabel}>{label}</label>}
+      </div>
+    )
+  }
+}
 
 FloatingLabelInput.propTypes = {label: PropTypes.string, className: PropTypes.string}
 
