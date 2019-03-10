8b372fbff9eef7eccc6e355af9faf915029e78c5
Abdulsemiu Atanda <semiu@live.com>
2019-03-10 14:43:08 +0100

    add newEvent reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,7 +3,8 @@ import {combineReducers} from 'redux'
 import auth from './authReducers'
 import events from './eventReducers'
 import eventActions from './eventActionsReducers'
+import newEvent from './newEventReducers'
 
-const rootReducer = combineReducers({auth, events, eventActions})
+const rootReducer = combineReducers({auth, events, eventActions, newEvent})
 
 export default rootReducer
---
+++ src/redux/reducers/newEventReducers.js
@@ -0,0 +1,23 @@
+import {asyncActionNames} from '../../helpers/reduxHelpers'
+import {NEW_EvENT} from '../actionTypes/eventActions'
+
+const initialState = {
+  creating: false,
+  event: {},
+  error: {status: false, message: ''}
+}
+
+const newEventReducers = (state = initialState, action) => {
+  switch(action.type) {
+    case asyncActionNames(NEW_EvENT).loading:
+      return {...state, creating: action.data}
+    case asyncActionNames(NEW_EvENT).failure:
+      return {...state, creating: false, error: {status: action.data.status, message: action.data.message}}
+    case asyncActionNames(NEW_EvENT).success:
+      return {...state, creating: false, error: {}, event: action.data}
+    default:
+      return state
+  }
+}
+
+export default newEventReducers
