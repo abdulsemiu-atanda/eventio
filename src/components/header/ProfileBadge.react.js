5942bc1da19e71e64106c192d934d72410734e8e
Abdulsemiu Atanda <semiu@live.com>
2019-03-10 22:47:50 +0100

    add log out functionality

--- src/components/header/ProfileBadge.react.js
+++ src/components/header/ProfileBadge.react.js
@@ -1,35 +1,42 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 
 import DropdownBase from '../shared/dropdown/DropdownBase.react'
-import {getToken, decodeToken} from '../../helpers/authHelpers'
+import {getToken, decodeToken, removeToken} from '../../helpers/authHelpers'
 import {constructInitials, fullName} from '../../helpers/tools'
 
 import './profileBadge.scss'
-
-const {user} = decodeToken(getToken('authToken'))
 
 class ProfileBadge extends React.Component {
   constructor() {
     super()
 
+    this.user = decodeToken(getToken('authToken')).user
     this.state = {showBadge: false}
+
+    this.logOut = this.logOut.bind(this)
     this.toggleBadgeDisplay = this.toggleBadgeDisplay.bind(this)
   }
 
   toggleBadgeDisplay() { this.setState({showBadge: !this.state.showBadge})}
 
+  logOut() {
+    removeToken(['authToken', 'refresh-token'])
+    this.props.history.push('/')
+  }
+
   render() {
     return (
       <div className='profile-badge'>
         <div className='initials'>
-          <span>{constructInitials(user)}</span>
+          <span>{constructInitials(this.user)}</span>
         </div>
-        <DropdownBase show={this.state.showBadge} launcher={() => (<div onClick={this.toggleBadgeDisplay} className='name'>{fullName(user)}</div>)}>
-          <span>Log out</span>
+        <DropdownBase show={this.state.showBadge} launcher={() => (<div onClick={this.toggleBadgeDisplay} className='name'>{fullName(this.user)}</div>)}>
+          <span onClick={this.logOut}>Log out</span>
         </DropdownBase>
       </div>
     )
   }
 }
 
-export default ProfileBadge
+export default withRouter(ProfileBadge)
